cb5f9644e11fabd1cd411098a1d0346122a1b8d5
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-10-13 16:19:45 -0300

    arquivo externo javascript

---
+++ interatividade.js
@@ -0,0 +1,38 @@
+var alturaTela = window.screen.height;
+var larguraTela = window.screen.width;
+
+function playSound(e) {
+  audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  tecla = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  tecla.classList.add("playing");
+}
+
+addEventListener("keydown", playSound);
+
+if (alturaTela <= 700 && larguraTela <= 400) {
+  addEventListener("touchstart", function (e) {
+    // Verifique se o evento de toque foi acionado em uma tecla
+    const target = e.target.closest(".key");
+    if (target) {
+      const keyCode = target.getAttribute("data-key");
+      playSound({ keyCode });
+    }
+  });
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", () => {
+    const keyCode = key.getAttribute("data-key");
+    playSound({ keyCode });
+  });
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
